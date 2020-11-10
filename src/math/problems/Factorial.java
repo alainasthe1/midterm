011169cb007d3a378c01af259efad717ab34de84
alainasthe1 <alainacb@gmail.com>
2020-11-09 20:15:51 -0500

    ok

--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -1,5 +1,7 @@
 package math.problems;
 
+import java.util.Scanner;
+
 public class Factorial {
 
     public static void main(String[] args) {
@@ -9,5 +11,26 @@ public class Factorial {
          *
          */
 
-    }
-}
+
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Enter the number:");
+
+                int num = scanner.nextInt();
+
+                int factorial = fact(num);
+                System.out.println("Factorial of entered number is: "+factorial);
+            }
+            static int fact(int n)
+            {
+                int output;
+                if(n==1){
+                    return 1;
+                }
+                output = fact(n-1)* n;
+                return output;
+            }
+        }
+
+
+
+
